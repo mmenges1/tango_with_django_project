808e757b3b8e9c0eef6537c036ce71112a4d8983
Makayla <mmenges3@gmail.com>
2019-01-23 10:49:13 +0000

    Populate_rango.py script

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
+    class Meta:
+        verbose_name_plural = 'Categories'
     def __str__(self):
         return self.name
 
