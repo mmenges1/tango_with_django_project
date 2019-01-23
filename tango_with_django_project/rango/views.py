68da577d610f73cdf6927efca3eba2feb13170f1
Makayla <mmenges3@gmail.com>
2019-01-23 10:14:59 +0000

    Chapter 3 commits

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
