32e6ae07fdaa2ca9921aa3792806de5ac6e0868f
Makayla <mmenges3@gmail.com>
2019-01-24 13:13:33 +0000

    Chapter 5 Complete

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -38,7 +38,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"], p["views"])
+            add_page(c, p["title"], p["url"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -53,8 +53,6 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
-    c.views = views
-    c.likes = likes
     c.save()
     return c
 
