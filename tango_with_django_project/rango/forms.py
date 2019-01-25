218d4d5434d034bfb373028813fa4331fbfc713e
Makayla <mmenges3@gmail.com>
2019-01-25 10:15:16 +0000

    Chapter 7 Exercises done

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -20,11 +20,11 @@ class PageForm(forms.ModelForm):
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
-        # If url is not empty and doesn't start with 'http://',
-        # then prepend 'http://'.
+
         if url and not url.startswith('http://'):
             url = 'http://' + url
             cleaned_data['url'] = url
+            
             return cleaned_data
 
     class Meta:
