eb4ab421f6d28bc69e5e5a84227754f7f5ba4248
Makayla <mmenges3@gmail.com>
2019-01-25 10:02:02 +0000

    Add category bugs fixed (Chapter 7)

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -2,14 +2,15 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=128,
+                            help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-class Meta:
-    model = Category
-    fields = ('name',)
+    class Meta:
+        model = Category
+        fields = ('name',)
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
@@ -26,6 +27,6 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
             return cleaned_data
 
-class Meta:
-    model = Page
-    exclude = ('category',)
+    class Meta:
+        model = Page
+        exclude = ('category',)
