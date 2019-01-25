952aa2ca61fc2c1863b5b7fec4e7d4ed88b9ae7c
Makayla <mmenges3@gmail.com>
2019-01-25 09:48:49 +0000

    Chapter 6 Exercise finished

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -30,3 +32,17 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            category = form.save(commit=True)
+            print(category, category.slug)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
